0bc6403ed9a736c4f482e1af5dfa638e3058d420
Jason Stieber <JrStieber@gmail.com>
2017-06-27 10:00:16 -0700

    First Commit, no real progress made just research and some nonsense code.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,20 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if(Number.isInteger(obj)){
+    return obj.toString();
+  }else if (Array.isArray(obj)) {
+    var strJSon = '[';
+    for(var i = 0; i<obj.length; i++){
+//      strJSon += key.toString() + ':' + obj[key].toString() + ',';
+    }strJSon += ']';
+    return strJSon;
+  }
+  else{
+      var strJSon = '{';
+      for(var key in obj){
+        strJSon += key.toString() + ':' + obj[key].toString() + ',';
+      }strJSon += '}';
+      return strJSon;
 };
