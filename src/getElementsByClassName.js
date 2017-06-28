77839f55a8c4eb1c4e7ad99f8a6672e0d3bb6c13
Jason Stieber <JrStieber@gmail.com>
2017-06-27 18:52:47 -0700

    Have an idea of the path to take and am now on my way

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,65 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var isClass(className, director){
+  if(className === directory.id){
+    return [directory];
+  }else{
+    return [];
+  }
+}
+var hasAunts(currentDir){
+  if(currentDir.parrent.nextSibling !== null){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+var getElementsByClassName = function(className,currentDir,goingUp
 ) {
-  // your code here
+
+  if(currentDir === undefined){
+    // first run though of the programs only gets here
+    goingUp = false;
+    elementsInThisTree = [];
+    currentDir = document;
+    return [].concat(getElementsByClassName(className, currentDir));
+  }
+  if(goingUp){
+    if(hasAunts(currentDir)){
+
+    }else{
+
+    }
+
+  }else{
+    if(currentDir.children.length >= 1){
+      return getElementsByClassName(className,currentDir.children[0]);
+    }else{
+      return isClass(className,CurrentDir).concat(getElementsByClassName();
+    }
+    if(currentDir.nextSibling !== null){
+
+    }else{
+      elementsInThisTree.concat(isClass(className, currentDir))
+    }
+  }
+//return getElementsByClassName(className,currentDir.childNodes)
+ //var thisLevelArray = [];
+
+  };
+
+
+
+
+
 };
+
+
+// parentNode
+// childNodes[nodenumber]
+// firstChild
+// lastChild
+// nextSibling
+// previousSibling
