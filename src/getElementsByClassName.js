78f051ff40cc8318465461e0c16efa2fb737c40d
Jason Stieber <JrStieber@gmail.com>
2017-06-27 19:54:18 -0700

    Time to Debug

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,24 +4,17 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var isClass(className, director){
+var isClass(className, directory){
   if(className === directory.id){
     return [directory];
   }else{
     return [];
   }
-}
-var hasAunts(currentDir){
-  if(currentDir.parrent.nextSibling !== null){
-    return true;
-  }else{
-    return false;
-  }
-}
+};
 
 var getElementsByClassName = function(className,currentDir,goingUp
 ) {
-
+  var classYes = isClass(className, currentDir);
   if(currentDir === undefined){
     // first run though of the programs only gets here
     goingUp = false;
@@ -30,36 +23,33 @@ var getElementsByClassName = function(className,currentDir,goingUp
     return [].concat(getElementsByClassName(className, currentDir));
   }
   if(goingUp){
-    if(hasAunts(currentDir)){
-
+    var parent = currentDir.parentNode;
+    var aunt = parentNode.nextSibling;
+    if(currentDir.parentNode.nextSibling !== null){
+      //checked is parentNode has the correct tag and the parents sibling
+      return classYes.concat(getElementsByClassName(className,aunt,false));
+    }else if(parent === null){
+      return classYes;
     }else{
-
+      return classYes.concat(getElementsByClassName(className, parent, true));
     }
-
   }else{
     if(currentDir.children.length >= 1){
+      // the search will progress untill we reach the yongest child on the left side
       return getElementsByClassName(className,currentDir.children[0]);
     }else{
-      return isClass(className,CurrentDir).concat(getElementsByClassName();
-    }
-    if(currentDir.nextSibling !== null){
+      if(currentDir.nextSibling !== null){
+        return classYes.concat(getElementsByClassName(className,currentDir.nextSibling,false);
 
-    }else{
-      elementsInThisTree.concat(isClass(className, currentDir))
+      }else{
+        return classYes.concat(getElementsByClassName(className,currentDir.parentNode,true);
+        //elementsInThisTree.concat(isClass(className, currentDir))
+      }
     }
   }
 //return getElementsByClassName(className,currentDir.childNodes)
  //var thisLevelArray = [];
-
-  };
-
-
-
-
-
 };
-
-
 // parentNode
 // childNodes[nodenumber]
 // firstChild
