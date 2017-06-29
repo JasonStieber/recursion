43af089d5711639108e64105a3f6720593df4062
Jason Stieber <JrStieber@gmail.com>
2017-06-29 16:16:06 -0700

    2nd atempt at building a working version of getElementsByClassName.js version 1 is still there but commented out.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -35,54 +35,87 @@ var isClass = function(className, directory){
 //   console.log(totalCount++);
 //   return [];
 // };
-
-
-var getElementsByClassName = function(className,currentDir,goingUp){
-  if(currentDir === undefined){
-    // first run though of the programs only gets here
-    console.log([].concat(getElementsByClassName(className, document,false)));
-    return [].concat(getElementsByClassName((className), document,false));
-    }
-  var arrIfCurrentDirIsClass = isClass(className, currentDir);
-  if(goingUp){
-    var parent = currentDir.parentNode;
-    if(parent === null){
-  //    console.log('4');
-      console.log("BASE CASE BABY!");
-      return arrIfCurrentDirIsClass;
-    }else if(parent.nextSibling !== null){
-      var newArr = arrIfCurrentDirIsClass.concat(isClass(className,parent));
-      return newArr.concat(getElementsByClassName(className,parent.nextSibling,false));
-    }else{
-  //    console.log('5');
-      //console.log("we are on the up path.");
-      return arrIfCurrentDirIsClass.concat(getElementsByClassName(className, parent, true));
-    }
+var isClass = function(className, node){
+  console.log(node);
+  if(node.classList.contains(className)){
+    return [node];
   }else{
-//    console.log('6');
-    // console.log("we are on the down path.");
-    if(currentDir.childNodes[0] !== undefined){
-      // the search will progress untill we reach the yongest child on the left side
-//      console.log('7');
-    //  console.log("we should be moving down to the next child here.");
-      return getElementsByClassName(className,(currentDir.childNodes[0]),false);
-    }else{
-  //    console.log('8');
-    //    console.log('there are no more children ----------------');
-      if(currentDir.nextSibling !== null){
-  //      console.log('9');
-    //    console.log("brothers and sisters");
-      return arrIfCurrentDirIsClass.concat(getElementsByClassName(className,currentDir.nextSibling,false));
-
-      }else{
-    //    console.log('10');
-    //    console.log('no more brothers and sisters............');
-        return [].concat(getElementsByClassName(className,currentDir,true));
-        //elementsInThisTree.concat(isClass(className, currentDir))
-      }
-    }
+    return [];
   }
 };
+
+var getElementsByClassName = function(className,currentDir){
+  // Node.childNodes
+  // Element.classList.contains()
+  // if(document.classList.contains(className)){
+  //    [document].concat(function(){
+  if(currentDir === undefined){
+    console.log('first step');
+    return getElementsByClassName(className,document.body);
+  }
+  var loopedArray = [];
+  if(currentDir.childNodes !== []){
+    console.log('we have children');
+    for(var i = 0; i < currentDir.childNodes.length; i++){
+      console.log('child ' + i);
+      loopedArray = loopedArray.concat(getElementsByClassName(className, currentDir.childNodes[i]));
+    }return isClass(className,currentDir).concat(loopedArray);
+  }else if(currentDir.childNodes === undefined){
+    console.log('this should never be displayed');
+  }else{
+    console.log('yongest child bitches!');
+    return isClass(className,currentDir);
+  }
+};
+
+//
+//
+// var getElementsByClassName = function(className,currentDir,goingUp){
+//   if(currentDir === undefined){
+//     // first run though of the programs only gets here
+//     console.log([].concat(getElementsByClassName(className, document,false)));
+//     return [].concat(getElementsByClassName((className), document,false));
+//     }
+//   var arrIfcurrentDirIsClass = isClass(className, currentDir);
+//   if(goingUp){
+//     var parent = currentDir.parentNode;
+//     if(parent === null){
+//   //    console.log('4');
+//       console.log("BASE CASE BABY!");
+//       return arrIfcurrentDirIsClass;
+//     }else if(parent.nextSibling !== null){
+//       var newArr = arrIfcurrentDirIsClass.concat(isClass(className,parent));
+//       return newArr.concat(getElementsByClassName(className,parent.nextSibling,false));
+//     }else{
+//   //    console.log('5');
+//       //console.log("we are on the up path.");
+//       return arrIfcurrentDirIsClass.concat(getElementsByClassName(className, parent, true));
+//     }
+//   }else{
+// //    console.log('6');
+//     // console.log("we are on the down path.");
+//     if(currentDir.childNodes[0] !== undefined){
+//       // the search will progress untill we reach the yongest child on the left side
+// //      console.log('7');
+//     //  console.log("we should be moving down to the next child here.");
+//       return getElementsByClassName(className,(currentDir.childNodes[0]),false);
+//     }else{
+//   //    console.log('8');
+//     //    console.log('there are no more children ----------------');
+//       if(currentDir.nextSibling !== null){
+//   //      console.log('9');
+//     //    console.log("brothers and sisters");
+//       return arrIfcurrentDirIsClass.concat(getElementsByClassName(className,currentDir.nextSibling,false));
+//
+//       }else{
+//     //    console.log('10');
+//     //    console.log('no more brothers and sisters............');
+//         return [].concat(getElementsByClassName(className,currentDir,true));
+//         //elementsInThisTree.concat(isClass(className, currentDir))
+//       }
+//     }
+//   }
+// };
 // parentNode
 // childNodes[nodenumber]
 // firstChild
